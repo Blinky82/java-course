a0bd45f7893cd51b4ba0d0d0e3bcc4e68f773661
Blinky82 <bstoianov@gmail.com>
2014-12-02 17:18:25 +0200

    homework2

---
+++ homework6.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * Created by blinky on 01.12.14.
+ */
+public class Cards {
+
+    public static void main(String args[]) {
+
+        String vid = new String["2"; "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K". "A"];
+
+        Scanner in = new Scanner(System.in);
+        Scanner input = new Scanner(System.in, "UTF-8");
+
+        System.out.println("Въведи произволна карта:");
+        vid = input.nextLine();
+
+        if (String vid == String[] {
+            System.out.println("Валидна карта:");
+        }
+
+        else {
+            System.out.println("Невалидна карта");
+        }
+
+    }
+}
